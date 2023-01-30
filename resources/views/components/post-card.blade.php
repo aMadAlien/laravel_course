723a79929be525ab500f37f64de0291590892dab
iren <irasibil2@gmail.com>
2023-01-30 17:50:16 +0200

    convert html page for single post

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -10,11 +10,7 @@
 
                         <div class="mt-8 flex flex-col justify-between">
                             <header>
-                                <div class="space-x-2">
-                                    <a href="/categories/{{ $post->category->slug }}"
-                                       class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                                       style="font-size: 10px">{{ $post->category->name }}</a>
-                                </div>
+                                <div class="space-x-2"><x-category-button :category="$post->category"/></div>
 
                                 <div class="mt-4">
                                     <h1 class="text-3xl">
@@ -41,7 +37,7 @@
                                 <div class="flex items-center text-sm">
                                     <img src="./images/lary-avatar.svg" alt="Lary avatar">
                                     <div class="ml-3">
-                                        <h5 class="font-bold">Lary Laracore</h5>
+                                        <h5 class="font-bold">{{ $post->author->name }}</h5>
                                         <h6>Mascot at Laracasts</h6>
                                     </div>
                                 </div>
