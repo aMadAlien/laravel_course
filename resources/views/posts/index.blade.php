b1f2eef4122889b4ecac735533a1af2b8a8c8ded
iren <irasibil2@gmail.com>
2023-02-01 17:48:21 +0200

    add pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,10 @@
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
             @if ($posts->count())
+                <!-- list of posts -->
                 <x-posts-grid :posts="$posts" />
+                <!-- pagination -->
+                {{ $posts->links() }}
             @else
                 <p class="text-center">No posts yet.</p>
             @endif
