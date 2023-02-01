bf6c89a43ed6284746e23988503f70cbb0787967
iren <irasibil2@gmail.com>
2023-02-01 16:42:35 +0200

    add capability in the same time to search and filter by category
    Extract a Category Dropdown Blade Component

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -35,5 +35,11 @@ class Post extends Model
                 ->where('title', 'like', '%' . request('search') . '%')
                 ->orWhere('body', 'like', '%' . request('search') . '%');
         });
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query->whereHas('category', fn ($query) =>
+                $query->where('slug', $category)
+            )
+        );
     }
 }
