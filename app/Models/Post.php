794bc3de0bfe48f6f694b6d9eb1fa167aad6f485
iren <irasibil2@gmail.com>
2023-02-03 11:54:54 +0200

    Activate the Comment Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,8 +9,6 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'excerpt', 'body'];
-
     protected $with = ['category', 'author'];
 
     public function comments()
